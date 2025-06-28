5a39f12c16fce05fec272d6a5cd7ea146501034d
Joao Gilberto Magalhaes <joao@byjg.com.br>
2025-06-28 04:41:52 -0500

    Switch default `exchange_type` to `FANOUT` in RabbitMQConnector and remove unused routing key properties.

--- src/RabbitMQConnector.php
+++ src/RabbitMQConnector.php
@@ -116,9 +116,9 @@ class RabbitMQConnector implements ConnectorInterface
      */
     protected function createQueue(AbstractConnection $connection, Pipe $pipe): AMQPChannel
     {
-        $pipe->setPropertyIfNull('exchange_type', AMQPExchangeType::DIRECT);
+        $pipe->setPropertyIfNull('exchange_type', AMQPExchangeType::FANOUT);
         $pipe->setPropertyIfNull(self::EXCHANGE, $pipe->getName());
-        $pipe->setPropertyIfNull(self::ROUTING_KEY, $pipe->getName());
+        // $pipe->setPropertyIfNull(self::ROUTING_KEY, $pipe->getName());
 
         // Get all queue properties
         $queueProperties = $pipe->getProperties();
@@ -126,12 +126,13 @@ class RabbitMQConnector implements ConnectorInterface
         // Handle dead letter queue if present
         $dlq = $pipe->getDeadLetter();
         if (!empty($dlq)) {
+            $dlq->withProperty('exchange_type', AMQPExchangeType::FANOUT);
             $channelDlq = $this->createQueue($connection, $dlq);
             $channelDlq->close();
 
             // Add dead letter properties
             $queueProperties['x-dead-letter-exchange'] = $dlq->getProperty(self::EXCHANGE, $dlq->getName());
-            $queueProperties['x-dead-letter-routing-key'] = $dlq->getName();
+            // $queueProperties['x-dead-letter-routing-key'] = $dlq->getName();
             // $queueProperties['x-message-ttl'] = $dlq->getProperty('x-message-ttl', 3600 * 72*1000);
             // $queueProperties['x-expires'] = $dlq->getProperty('x-expires', 3600 * 72*1000 + 1000);
         }
@@ -159,7 +160,7 @@ class RabbitMQConnector implements ConnectorInterface
         */
         $channel->exchange_declare($pipe->getProperty(self::EXCHANGE, $pipe->getName()), $pipe->getProperty('exchange_type'), false, true, false);
 
-        $channel->queue_bind($pipe->getName(), $pipe->getProperty(self::EXCHANGE, $pipe->getName()), $pipe->getProperty(self::ROUTING_KEY, $pipe->getName()));
+        $channel->queue_bind($pipe->getName(), $pipe->getProperty(self::EXCHANGE, $pipe->getName()));
 
         return $channel;
     }
